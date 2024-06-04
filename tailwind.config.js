3984e49fd9da62b8bd90e7170b20fc6e6468de3a
akbarryyan <akbarrayyan182@gmail.com>
2024-06-04 19:31:25 +0700

    Fix Responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,12 @@ module.exports = {
     fontFamily: {
       body: ["Lato"],
     },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "960px",
+      xl: "1200px",
+    },
     extend: {},
   },
   plugins: [],
